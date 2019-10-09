39c748c9d2954ce2c6771450033461c91c80aacf
lgcooci <cooci_tz@163.com>
2019-10-09 15:18:25 +0800

    更新编译文件

--- objc4-756.2编译/objc4-756.2可编译/objc-debug/main.m
+++ objc4-756.2编译/objc4-756.2可编译/objc-debug/main.m
@@ -1,8 +1,8 @@
 //
 //  main.m
-//  KCDebugObjc
+//  objc-debug
 //
-//  Created by Cooci on 2019/10/8.
+//  Created by Cooci on 2019/10/9.
 //
 
 #import <Foundation/Foundation.h>
@@ -10,8 +10,8 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
-        NSObject *objc = [NSObject alloc];
+        NSObject *object = [NSObject alloc];
+        NSLog(@"Hello, World! %@",object);
     }
     return 0;
 }
